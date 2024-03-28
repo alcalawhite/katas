e606b6159d973f5b3b99c68ff531083278c35766
alcalawhite <vale_alcala7@hotmail.com>
2024-03-28 12:29:24 -0600

    Kata Boolean to String
    Declared an if statement to check if the parameter given to the function
    is true or false. The function returns the string of the boolean value
    given

---
+++ convertABooleanToString.py
@@ -0,0 +1,12 @@
+"""""
+Implement a function which convert
+the given boolean value into its string representation.
+"""""
+
+def bool_to_string(b):
+    if b:
+        return ("True")
+    else:
+        return ("False")
+
+bool_to_string(False)
